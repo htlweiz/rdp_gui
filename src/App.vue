8f2814d212a1e83d6b704b60f5bf7427914342be
denisepostl <postl190070@sr.htlweiz.at>
2023-09-25 11:03:50 +0200

    Command Pattern

--- src/App.vue
+++ src/App.vue
@@ -9,14 +9,59 @@ import { Value } from './scripts/value'
 </script>
 
 <script lang="ts">
+// Command interface
+interface Command {
+  execute(value: string): void;
+}
+
+// Command to handle 'type' filter
+class TypeFilterCommand implements Command {
+  private component: any;
+
+  constructor(component: any) {
+    this.component = component;
+  }
+
+  execute(value: string): void {
+    this.component.filter_type = this.component.getTypeId(value);
+    console.log('Update typeid', this.component.filter_type);
+  }
+}
+
+// Command to handle 'start' filter
+class StartFilterCommand implements Command {
+  private component: any;
+
+  constructor(component: any) {
+    this.component = component;
+  }
+
+  execute(value: string): void {
+    this.component.filter_start = value;
+  }
+}
+
+// Command to handle 'end' filter
+class EndFilterCommand implements Command {
+  private component: any;
+
+  constructor(component: any) {
+    this.component = component;
+  }
+
+  execute(value: string): void {
+    this.component.filter_end = value;
+  }
+}
+
 export default {
   data() {
     return {
       values: new Array<Value>(),
       value_types: new Array<ValueType>(),
-      filter_start : '',
-      filter_end : '',
-      filter_type : ''
+      filter_start: '',
+      filter_end: '',
+      filter_type: ''
     }
   },
   mounted() {
@@ -37,34 +82,46 @@ export default {
       return return_value
     },
     update_search(args: string[]) {
-      console.log('New search arguemnts', args)
-      this.filter_end=''
-      this.filter_start=''
-      this.filter_type=''
-      for (var i = 0; i < args.length; i++) {
-        const command = args[i]
-        console.log('handling command', command)
-        const command_and_args = args[i].split(':')
-        if (command_and_args.length == 2) {
-          const key = command_and_args[0]
-          const value = command_and_args[1]
-          if (key == 'type') {
-            this.filter_type = this.getTypeId(value)
-            console.log('Update typeid', this.filter_type)
-            continue
-          } else if (key == 'start') {
-            this.filter_start = value
-            continue
-          } else if (key == 'end') {
-            this.filter_end = value
-            continue
+      console.log('New search arguments', args);
+
+      // Clear filters
+      this.filter_end = '';
+      this.filter_start = '';
+      this.filter_type = '';
+
+      for (let i = 0; i < args.length; i++) {
+        const command_and_args = args[i].split(':');
+        if (command_and_args.length === 2) {
+          const key = command_and_args[0];
+          const value = command_and_args[1];
+
+          switch (key) {
+            case 'type':
+              const typeCommand = new TypeFilterCommand(this);
+              typeCommand.execute(value);
+              break;
+
+            case 'start':
+              const startCommand = new StartFilterCommand(this);
+              startCommand.execute(value);
+              break;
+
+            case 'end':
+              const endCommand = new EndFilterCommand(this);
+              endCommand.execute(value);
+              break;
+
+            default:
+              console.log('Ignoring command', args[i]);
           }
+        } else {
+          console.log('Ignoring command', args[i]);
         }
-        console.log('Ignoring command', command)
       }
+
       this.get_values().then((result) => {
-        this.values = result
-      })
+        this.values = result;
+      });
     },
     get_types() {
       axios
@@ -79,7 +136,7 @@ export default {
     get_values() {
       const promise = new Promise<Value[]>((accept, reject) => {
         const url = '/api/value/'
-        var params : { [key: string]: string } = {}
+        var params: { [key: string]: string } = {}
         if (this.filter_type != '') {
           params['type_id'] = this.filter_type
         }
@@ -87,13 +144,12 @@ export default {
           params['end'] = this.filter_end
         }
         if (this.filter_start != '') {
-          params['start']=this.filter_start
+          params['start'] = this.filter_start
         }
         console.log('Trying to get url', url)
         axios
           .get(url, { params: params })
           .then((result) => {
-            // console.log('Got values: ', result.data)
             accept(result.data)
           })
           .catch((error) => {
