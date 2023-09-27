c207d6185bf487dfc41bb4c06e24087da5a6415a
mderler <derler190063@sr.htlweiz.at>
2023-09-27 20:29:35 +0000

    Improve device gui

--- src/components/DeviceDisplay.vue
+++ src/components/DeviceDisplay.vue
@@ -1,32 +1,30 @@
 <script lang="ts">
-import axios from 'axios';
+import axios from 'axios'
 
-import SingleDeviceDisplay from './SingleDeviceDisplay.vue';
-import SingleTypeDisplay from './SingleTypeDisplay.vue';
-import { Device } from '@/scripts/device';
+import SingleDeviceDisplay from './SingleDeviceDisplay.vue'
+import { Device } from '@/scripts/device'
 export default {
-    props: ['devices'],
-    data() {
-        return {
-            newDevice: new Device(),
-            editor_hidden: true
-        };
+  props: ['devices'],
+  data() {
+    return {
+      newDevice: new Device(),
+      editor_hidden: true
+    }
+  },
+  methods: {
+    toggleEditor() {
+      this.editor_hidden = !this.editor_hidden
     },
-    methods: {
-        toggleEditor() {
-            this.editor_hidden = !this.editor_hidden;
-        },
-        add_device() {
-          console.log("add device", this.newDevice)
-          axios.put("/api/device/0/", this.newDevice)
-                .then((result) => {
-                    console.log(result)
-                    this.$emit("update_device")
-                })
-        }
-    },
-    components: { SingleTypeDisplay, SingleDeviceDisplay },
-    emits: ['update_device']
+    add_device() {
+      console.log('add device', this.newDevice)
+      axios.put('/api/device/0/', this.newDevice).then((result) => {
+        console.log(result)
+        this.$emit('update_device')
+      })
+    }
+  },
+  components: { SingleDeviceDisplay },
+  emits: ['update_device']
 }
 </script>
 
@@ -39,12 +37,19 @@ export default {
     </span>
   </div>
   <span v-if="!editor_hidden">
-    <SingleDeviceDisplay :device-prop="device" v-for="device in devices" :key="device" @update_device="$emit('update_device')"/>
-    <p>Add new device</p>
-    <div class="row rounded mt-1 mb-0 p-0 ">
-      <input class="col bg-secondary rounded me-1" v-model="newDevice.name"/>
-      <input class="col bg-secondary rounded" v-model="newDevice.device"/>
-      <button class="col bg-primary text-end rounded mb-1" v-on:click="add_device">submit </button>
+    <SingleDeviceDisplay
+      :device-prop="device"
+      v-for="device in devices"
+      :key="device"
+      @update_device="$emit('update_device')"
+    />
+    <span>
+      <b>Add new device:</b>
+    </span>
+    <div class="row rounded mt-1 mb-0 p-0">
+      <input class="col bg-secondary rounded me-1" v-model="newDevice.name" />
+      <input class="col bg-secondary rounded" v-model="newDevice.device" />
+      <button class="col bg-primary text-end rounded mb-1" v-on:click="add_device">submit</button>
     </div>
   </span>
 </template>
--- src/components/SingleTypeDisplay.vue
+++ src/components/SingleTypeDisplay.vue
@@ -1,46 +1,53 @@
 <script lang="ts">
-
-import { ValueType } from '@/scripts/value_type';
-import axios from 'axios';
+import { ValueType } from '@/scripts/value_type'
+import axios from 'axios'
 
 export default {
-    props: ["type_prop"],
-    data() {
-        return {
-            edit_type: new ValueType(),
-            changed: false
-        }
+  props: ['type_prop'],
+  data() {
+    return {
+      edit_type: new ValueType(),
+      changed: false
+    }
+  },
+  mounted() {
+    console.log('Mounted with ', this.type_prop)
+    this.edit_type.id = this.type_prop.id
+    this.edit_type.type_name = this.type_prop.type_name
+    this.edit_type.type_unit = this.type_prop.type_unit
+  },
+  methods: {
+    update_changed() {
+      this.changed =
+        this.type_prop.type_name != this.edit_type.type_name ||
+        this.type_prop.type_unit != this.edit_type.type_unit
+      console.log('Changed is now', this.changed)
     },
-    mounted() {
-        console.log("Mounted with ", this.type_prop)
-        this.edit_type.id=this.type_prop.id
-        this.edit_type.type_name=this.type_prop.type_name
-        this.edit_type.type_unit=this.type_prop.type_unit
-    },
-    methods: {
-        update_changed() {
-            this.changed = ( (this.type_prop.type_name != this.edit_type.type_name) || 
-                             (this.type_prop.type_unit != this.edit_type.type_unit) )
-            console.log("Changed is now",this.changed)
-        },
-        update_type() {
-            axios.put("/api/type/"+this.edit_type.id+"/",this.edit_type)
-                .then((result) => {
-                    console.log(result)
-                    this.$emit("update_device")
-                })
-
-        }
-    },
-    emits: ["update_device"]
+    update_type() {
+      axios.put('/api/type/' + this.edit_type.id + '/', this.edit_type).then((result) => {
+        console.log(result)
+        this.$emit('update_device')
+      })
+    }
+  },
+  emits: ['update_device']
 }
 </script>
 
 <template>
-<div class="row rounded mt-1 mb-0 p-0 ">
-    <input class="col bg-secondary rounded me-1" v-model="edit_type.type_name" v-on:change="update_changed" />
-    <input class="col bg-secondary rounded" v-model="edit_type.type_unit" v-on:change="update_changed"/>
-    <button class="col bg-primary text-end rounded mb-1" v-if="changed" v-on:click="update_type()">submit </button>
-</div>
-
+  <div class="row rounded mt-1 mb-0 p-0">
+    <input
+      class="col bg-secondary rounded me-1"
+      v-model="edit_type.type_name"
+      v-on:change="update_changed"
+    />
+    <input
+      class="col bg-secondary rounded"
+      v-model="edit_type.type_unit"
+      v-on:change="update_changed"
+    />
+    <button class="col bg-primary text-end rounded mb-1" v-if="changed" v-on:click="update_type()">
+      submit
+    </button>
+  </div>
 </template>
